fdfcd812e39a2b6e2662447a4ed4dd1df3d49586
SRenuuu <sandulrenuja@gmail.com>
2023-09-27 20:01:18 +0530

    Create spring-boot project for user-auth-service

---
+++ backend/user-auth-management-service/src/main/java/lk/ucsc/sricare/userauthmanagementservice/UserAuthManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.ucsc.sricare.userauthmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAuthManagementServiceApplication.class, args);
+    }
+
+}
---
+++ backend/user-auth-management-service/src/test/java/lk/ucsc/sricare/userauthmanagementservice/UserAuthManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ucsc.sricare.userauthmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
