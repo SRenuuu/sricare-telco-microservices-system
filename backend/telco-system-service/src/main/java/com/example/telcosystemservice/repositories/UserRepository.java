4ee9eb9cf15356d71ff1da4204da8aba041f594a
Neethamadhu Madurasinghe <neethamadhu.ma@gmail.com>
2023-09-28 19:16:28 +0530

    Add findById method

--- backend/telco-system-service/src/main/java/com/example/telcosystemservice/repositories/UserRepository.java
+++ backend/telco-system-service/src/main/java/com/example/telcosystemservice/repositories/UserRepository.java
@@ -2,7 +2,10 @@ package com.example.telcosystemservice.repositories;
 
 import com.example.telcosystemservice.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findById(UUID id);
 }
