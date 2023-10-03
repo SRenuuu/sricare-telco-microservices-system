5d49341933e837169f9b2bca06df1ac8a28ed91a
DivakaTharindaPerera <thimiradivaka@gmail.com>
2023-10-03 12:23:36 +0530

    api-gateway init done

---
+++ backend/web-mobile-api-service/web-mobile-api-service/src/main/java/lk/ucsc/sricare/webmobileapiservice/WebMobileApiServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.ucsc.sricare.webmobileapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMobileApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMobileApiServiceApplication.class, args);
+	}
+
+}
---
+++ backend/web-mobile-api-service/web-mobile-api-service/src/test/java/lk/ucsc/sricare/webmobileapiservice/WebMobileApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ucsc.sricare.webmobileapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMobileApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
